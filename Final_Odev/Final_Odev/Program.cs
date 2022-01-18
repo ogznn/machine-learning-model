80d9805a9cb72efc42ffc854565f773b6c405bcf
ogznn <oguzhanersoy55@gmail.com>
2022-01-18 22:46:07 +0300

    Machine Learning Model Example
    We predict data from football matches using Machine Learning Model.

---
+++ Final_Odev/Final_Odev/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Final_Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String quit;
+            String HomeTeam;
+            String AwayTeam;
+            float FTHomeGol;
+            float FTAwayGol;
+            float AwayShot;
+            float HomeShot;
+            float AwayCorner;
+
+            while (true) //Kullanıcı programa "cikis" yazana kadar döngüyü devam ettiriyor.
+            {
+                Console.WriteLine("******************Skor Tahmin Etme Programi******************");
+            
+            startHomeTeam: 
+                Console.Write("Ev Sahibi takimi girin: ");
+                HomeTeam = Console.ReadLine(); //HomeTeam için kullancıdan veri alıyoruz.
+
+                if (HomeTeam == "" || HomeTeam == " ")
+                {
+                    Console.WriteLine("Bos veri girilemez!");
+                    goto startHomeTeam; //Eğer boş veri girerse başa dönmesi için goto kullandık.
+                }
+
+            startAwayTeam:
+                Console.Write("Rakip takimi girin: ");
+                AwayTeam = Console.ReadLine(); //AwayTeam için kullancıdan veri alıyoruz.
+
+                if (AwayTeam == "" || AwayTeam == " ")
+                {
+                    Console.WriteLine("Bos veri girilemez!");
+                    goto startAwayTeam; //Eğer boş veri girerse başa dönmesi için goto kullandık.
+                }
+
+            startFTHomeGol:
+                Console.Write("Ev sahibi takimin attigi toplam gol sayisi: ");
+                FTHomeGol = float.Parse(Console.ReadLine()); //FTHomeGol için kullancıdan veri alıyoruz.
+
+                if (FTHomeGol < 0)
+                {
+                    Console.WriteLine("Sayi negatif olamaz!");
+                    goto startFTHomeGol; //Eğer negatif bir deger girerse başa dönmesi için goto kullandık.
+                }
+
+            startFTAwayGol:
+                Console.Write("Rakip takimin attigi toplam gol sayisi: ");
+                FTAwayGol = float.Parse(Console.ReadLine()); //FTAwayGol için kullancıdan veri alıyoruz.
+
+                if (FTAwayGol < 0)
+                {
+                    Console.WriteLine("Sayi negatif olamaz!");
+                    goto startFTAwayGol; //Eğer negatif bir deger girerse başa dönmesi için goto kullandık.
+                }
+
+            startHomeShot:
+                Console.Write("Ev sahibi takimin attigi sut sayisi: ");
+                HomeShot = float.Parse(Console.ReadLine()); //HomeShot için kullancıdan veri alıyoruz.
+
+                if (HomeShot < 0)
+                {
+                    Console.WriteLine("Sayi negatif olamaz!");
+                    goto startHomeShot; //Eğer negatif bir deger girerse başa dönmesi için goto kullandık.
+                }
+
+            startAwayShot:
+                Console.Write("Rakip takimin attigi sut sayisi: ");
+                AwayShot = float.Parse(Console.ReadLine()); //AwayShot için kullancıdan veri alıyoruz.
+
+                if (AwayShot < 0)
+                {
+                    Console.WriteLine("Sayi negatif olamaz!");
+                    goto startAwayShot; //Eğer negatif bir deger girerse başa dönmesi için goto kullandık.
+                }
+
+            startAwayCorner:
+                Console.Write("Rakip takimin attigi korner sayisi: ");
+                AwayCorner = float.Parse(Console.ReadLine()); //AwayCorner için kullancıdan veri alıyoruz.
+
+                if (AwayCorner < 0)
+                {
+                    Console.WriteLine("Sayi negatif olamaz!");
+                    goto startAwayCorner; //Eğer negatif bir deger girerse başa dönmesi için goto kullandık.
+                }
+
+                MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
+                {
+                    HomeTeam = HomeTeam,
+                    AwayTeam = AwayTeam,
+                    FTHomeGol = FTHomeGol,
+                    FTAwayGol = FTAwayGol,
+                    HomeShot = HomeShot,
+                    AwayShot = AwayShot,
+                    AwayCorner = AwayCorner,
+                };
+
+                // Make a single prediction on the sample data and print results
+                var predictionResult = MLModel1.Predict(sampleData);
+
+                Console.WriteLine($"HomeTeam: {HomeTeam}");
+                Console.WriteLine($"AwayTeam: {AwayTeam}");
+                Console.WriteLine($"FTHomeGol: {FTHomeGol}");
+                Console.WriteLine($"FTAwayGol: {FTAwayGol}");
+                Console.WriteLine($"HomeShot: {HomeShot}");
+                Console.WriteLine($"AwayShot: {AwayShot}");
+                Console.WriteLine($"AwayCorner: {AwayCorner}");
+
+
+                Console.WriteLine($"\n\nTahmin edilen ev sahibi takimin attigi korner sayisi: {predictionResult.Score}\n\n");
+                Console.Write("Cikmak icin cikis, devam etmek icin herhangi bir sey yaziniz. ");
+                quit = Console.ReadLine();
+                if (quit == "cikis") break; //Eger kullanıcı işlemin sonunda "cikis" yazarsa program sonlanacak.
+
+            }
+                Console.ReadKey();
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Yapay Zeka Final Ödevi
+
+Ödevde E2021new verilerindeki makine öğrenmesini gerçekleştirdikten sonra HomeCorner alanıyla ilgili tahmin etme işlemini gerçekleştiren bir konsol uygulaması hazırladık. Makine, ilgili alanlara veri girişi yapıtğınızda HomeCorner alanını tahmin etmeye çalışıyor. Uygulamayı sürekli tekrar edecek şekilde ayarladık. Eğer kullanıcı çıkmak isterse işlemin sonunda "cikis" yazmasi ile program sonlanıyor. Dinamik bir veri girişi sağladık yani string alanlara boş değer, sayı girilmesi gereken alanlara negatif değer girilemiyor.
+
